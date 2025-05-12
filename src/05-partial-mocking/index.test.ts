237d64b88354fbc4f343bc5acecc31f33bda537f
IlhomMax <parnasmi@gmail.com>
2025-05-13 00:30:55 +0500

    test: add and implement tests for partial mocking, lib api, and node api

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,20 +1,42 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const actual = jest.requireActual<typeof import('./index')>('./index');
+  return {
+    ...actual,
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
+  };
 });
 
 describe('partial mocking', () => {
+  let consoleSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleSpy.mockRestore();
+  });
+
   afterAll(() => {
     jest.unmock('./index');
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
     // Write your test here
+    mockOne();
+    mockTwo();
+    mockThree();
+
+    expect(consoleSpy).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    unmockedFunction();
+    expect(consoleSpy).toHaveBeenCalledWith('I am not mocked');
   });
 });
--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,48 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => {
+  const actual = jest.requireActual('lodash');
+  return {
+    ...actual,
+    throttle: (fn: any, _wait: number) => fn, // Replace throttle with identity function for testing
+  };
+});
 
 describe('throttledGetDataFromApi', () => {
+  const mockGet = jest.fn();
+  const mockCreate = axios.create as jest.Mock;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockCreate.mockReturnValue({ get: mockGet });
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    mockGet.mockResolvedValue({ data: 'mocked data' });
+
+    await throttledGetDataFromApi('/posts');
+
+    expect(mockCreate).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    mockGet.mockResolvedValue({ data: 'mocked data' });
+
+    await throttledGetDataFromApi('/todos/1');
+
+    expect(mockGet).toHaveBeenCalledWith('/todos/1');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    mockGet.mockResolvedValue({ data: { id: 1, title: 'test' } });
+
+    const result = await throttledGetDataFromApi('/users/1');
+
+    expect(result).toEqual({ id: 1, title: 'test' });
   });
 });
